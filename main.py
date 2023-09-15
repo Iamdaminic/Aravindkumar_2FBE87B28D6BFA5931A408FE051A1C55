3aa8c92f71346ac2b5c123cd66f91b25b61a9f4e
Iamdaminic <144986046+Iamdaminic@users.noreply.github.com>
2023-09-15 19:24:06 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
-# 1.1 Implement a recursive function to calculate  the factorial of a given number.
-
-"""
-1!=1*1
-2!=2*1!--->2*1
-3!=3*2!--->3*2*1
-.
-.
-10!=10*9!--->10*9*8*7*6*5*4*3*2*1
-
-formula=n*(n-1)!
-"""
-
-
+#1-1 Implement a recursive function to calculate the factorial of a given number
 def fact_rec(n):
- if n==0 or n==1: 
-   return 1
- else:
-   return n*fact_rec(n-1)
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * fact_rec(n - 1)
 
-number =int(input("Enter a value:"))
-res =fact_rec(number)
-
-print("The factorial of {} is    {}".format(number,res))
- 
+number = int(input("Enter a value: "))
+res = fact_rec(number)
+print("The factorial of {} is {}.".format(number, res))
